296bf137655393ece6cff5cedfae553750c67288
Matt Styles <matt@veryfizzyjelly.com>
2014-08-31 08:38:01 +0100

    #add - state transitions

--- urban-login.js
+++ urban-login.js
@@ -5,6 +5,8 @@
         _showing: false,
         _loading: false,
 
+        loadEl: null,
+
         eventDelegates: {
             down: 'downAction',
             up: 'upAction'
@@ -13,13 +15,15 @@
 
         ready: function() {
             console.log( 'Urban-login ready to rock' );
+
+            this.loadEl = this.querySelector( '#loading' );
         },
 
 
         downAction: function( event ) {
             if ( !this._showing ) return;
 
-            
+
         },
 
         upAction: function( event ) {
@@ -35,10 +39,26 @@
         },
 
         hide: function() {
-            if ( !this.showing || !this._loading ) return;
+            if ( !this._showing || !this._loading ) return;
 
             this.$.login.classList.add( 'disable', 'transparent' );
             this._showing = false;
+        },
+
+        showLoading: function() {
+            if ( !this.loadEl || this.loading ) return;
+
+            this._loading = true;
+            this.hide();
+            this.loadEl.classList.remove( 'disable', 'transparent' );
+        },
+
+        hideLoading: function() {
+            if ( !this.loadEl || !this._loading ) return;
+
+            this.loadEl.classList.add( 'disable', 'transparent' );
+            this._loading = false;
+            this.show();
         }
 
     });
